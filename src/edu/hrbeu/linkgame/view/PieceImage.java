16b5b28a661e41e4a295f76266ac0058e80a9c2c
ShiningBoy <huhaijingxin@163.com>
2014-11-13 21:46:58 +0800

    Piece and PieceImage

--- src/edu/hrbeu/linkgame/view/PieceImage.java
+++ src/edu/hrbeu/linkgame/view/PieceImage.java
@@ -1,7 +1,7 @@
 package edu.hrbeu.linkgame.view;
 
 import android.graphics.Bitmap;
-
+//方块上的图片，以及图片的id
 public class PieceImage {
 	
 	private Bitmap image;
@@ -12,4 +12,22 @@ public class PieceImage {
 		this.image = image;
 		this.imageId = imageId;
 	}
+
+	public Bitmap getImage() {
+		return image;
+	}
+
+	public void setImage(Bitmap image) {
+		this.image = image;
+	}
+
+	public int getImageId() {
+		return imageId;
+	}
+
+	public void setImageId(int imageId) {
+		this.imageId = imageId;
+	}
+	
+	
 }
